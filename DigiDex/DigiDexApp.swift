a8c51c715c34d58d672ac3bbe81763428f099ee1
Osasu Sanachez <osasusan@gmail.com>
2026-06-09 03:29:40 +0200

    Initial Commit

---
+++ DigiDex/DigiDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiDexApp.swift
+//  DigiDex
+//
+//  Created by Osasu sanchez on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct DigiDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
